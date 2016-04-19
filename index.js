8665bad8352ec9139b6fbb1bfe3d5874660c2a69
David Bautista <dbautistav@gmail.com>
2016-04-18 23:43:43 -0500

    Improved.

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 /**
  * A Bot for Slack!
  */
+var fetch = require('node-fetch');
 
 
 /**
@@ -34,14 +35,13 @@ if (process.env.MONGOLAB_URI) {
     };
 } else {
     config = {
-        json_file_store: ((process.env.TOKEN)?'./db_slack_bot_ci/':'./db_slack_bot_a/'), //use a different name if an app or CI
+        json_file_store: ((process.env.TOKEN)?'./db_slack_bot_ci/':'./db_slack_bot_a/') //use a different name if an app or CI
     };
 }
 
 /**
  * Are being run as an app or a custom integration? The initialization will differ, depending
  */
-
 if (process.env.TOKEN || process.env.SLACK_TOKEN) {
     //Treat this as a custom integration
     var customIntegration = require('./lib/custom_integrations');
@@ -82,16 +82,69 @@ controller.on('rtm_close', function (bot) {
 // BEGIN EDITING HERE!
 
 controller.on('bot_channel_join', function (bot, message) {
-    bot.reply(message, "I'm here!")
+    bot.reply(message, "Hello here!")
 });
 
-controller.hears('hello', 'direct_message', function (bot, message) {
-    bot.reply(message, 'Hello!');
-});
 
+//controller.hears('hello', 'direct_message', function (bot, message) {
+//    bot.reply(message, 'Hello!');
+//});
+
+// FIXME: move this to its own file and 'require' it!
+function ehecatlCore(callback) {
+    fetch("http://www.aire.df.gob.mx/default.php")
+        .then(function(res) {
+            return res.text();
+        })
+        .then(function(body) {
+            ////console.log(body);
+            var salidas = JSON.stringify(body.replace(/</g, "&lt;").replace(/>/g, "&gt;").replace(/"/i, "_").replace(/'/i, "-"));
+
+            var calidadDelAireDivId = "iconos-recomendaciones-calidad-aire";
+            var indiceUVImgId = "iconos-uv";
+
+            var calidadIdx = salidas.indexOf(calidadDelAireDivId) + calidadDelAireDivId.length + 1;
+            var indiceIdx = salidas.indexOf(indiceUVImgId) + indiceUVImgId.length + 1;
+
+            var calidadRegExp = /.*(.png)/gi;
+            var indiceRegExp = /(\d)+/gi;
+
+            var calidadFragmentStr = salidas.slice(calidadIdx, calidadIdx + 10);
+            var indiceFragmentStr = salidas.slice(indiceIdx, indiceIdx + 10);
+
+            var calidadHelper = calidadFragmentStr.match(calidadRegExp);
+            var calidadHelperMatchedZero = calidadHelper[0].match(calidadRegExp)[0];
+
+            var calidadResult = calidadHelperMatchedZero.slice(0, calidadHelperMatchedZero.indexOf("."));
+            var indiceResult = indiceFragmentStr.match(indiceRegExp)[0];
+
+            //console.log("Calidad del aire:", calidadResult);
+            //console.log("Indice UV:", indiceResult);
+
+            var outputObj = {
+                calidad: "Calidad del aire: " + calidadResult,
+                indice: "Indice UV: " + indiceResult,
+                publico: (indiceResult > 5) ? "@channel: " : ""
+            };
+
+            callback(outputObj);
+
+        });
+}
+
+controller
+    .hears(
+        ["echo", "how's out there?"],
+        ["direct_mention", "mention", "direct_message"],
+        function (bot, message) {
+            ehecatlCore(function (responseObj) {
+                bot.reply(message, responseObj);
+            });
+        }
+    );
 
 /**
- * AN example of what could be:
+ * An example of what could be:
  * Any un-handled direct mention gets a reaction and a pat response!
  */
 //controller.on('direct_message,mention,direct_mention', function (bot, message) {
